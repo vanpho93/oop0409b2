267aed9ae5e5a321a642cc0db0c2a3bc4d009af3
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-18 17:38:47 +0700

    a

---
+++ a.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const teo = new Person('Teo Nguyen', 10);
+const ti = new Person('Ti Nguyen', 10);
+const tun = new Person('Tun Nguyen', 10);
