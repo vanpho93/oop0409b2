4c11758aa654c8176e0cb0ceed424fa2f18e2746
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-18 18:37:33 +0700

    constructor

--- e.js
+++ e.js
@@ -2,8 +2,28 @@ class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
-        this.sayHello = function(){
-            console.log('Xin chao, toi la ' + this.name);
-        }
+    }
+
+    sayGoodbye() {
+        console.log('Tam biet');
+    }
+
+    sayHello(){
+        console.log('Xin chao, toi la ' + this.name);
     }
 }
+
+class Child extends Person {
+    constructor(name, age, toy) {
+        super(name, age);
+        this.toy = toy;
+    }
+    sayHello() {
+        super.sayHello();
+        console.log('Xin chao, em la ' + this.name + ' em thich choi ' + this.toy);
+    }
+}
+
+const teo = new Child('Teo Nguyen', 10, 'May bay');
+teo.sayHello();
+teo.sayGoodbye();
