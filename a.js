5d58e670402c84c7607a38f2441864ca0837c16f
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-18 17:47:36 +0700

    prototype

--- a.js
+++ a.js
@@ -5,6 +5,23 @@ class Person {
     }
 }
 
+//2 cach mo rong. c1 -> tac dong truc tiep 
+
+Person.prototype.sayHello = function() {
+    console.log('Xin chao, toi la ' + this.name);
+}
+
 const teo = new Person('Teo Nguyen', 10);
 const ti = new Person('Ti Nguyen', 10);
 const tun = new Person('Tun Nguyen', 10);
+
+ti.sayHello();
+
+// console.log(teo);
+// teo.height = 200;
+// console.log(teo.height);
+
+// teo.sayHello = function() {
+//     console.log('Xin chao, toi la ' + this.name);
+// }
+// teo.sayHello();
